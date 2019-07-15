8a4b17fd073aa037c7d1d9c7740a0e16523dee8b
manojrege <manoj.rege@emnify.com>
2019-07-15 13:45:34 +0200

    Create anchor

--- emulate.py
+++ emulate.py
@@ -9,3 +9,14 @@ def flush_dnctl():
     except subprocess.CalledProcessError as e:
         print(e.output)
         return False
+
+def create_anchor(anchor_name):
+    CREATE_ANCHOR_COMMAND = '(cat /etc/pf.conf && echo \'dummynet-anchor "'+anchor_name+'"\' && echo \'anchor "'+anchor_name+'"\') | pfctl -q -f -'
+    try:
+        output = subprocess.check_output(CREATE_ANCHOR_COMMAND, shell=True)
+        print(output)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        return False
+
