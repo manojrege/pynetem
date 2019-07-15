564d80235b6fcfd2c504c2b930c04422a9590cbb
manojrege <manoj.rege@emnify.com>
2019-07-15 16:35:42 +0200

    configure anchor

--- emulate.py
+++ emulate.py
@@ -20,3 +20,27 @@ def create_anchor(anchor_name):
         print(e.output)
         return False
 
+def configure_anchor(anchor_name, type):
+
+    try:
+        if type == 'simplex':
+            CONFIGURE_ANCHOR = 'echo "dummynet in all pipe 1" | sudo pfctl -a '+anchor_name+' -f -'
+            output = subprocess.check_output(CONFIGURE_ANCHOR, shell=True)
+            print(output)
+        elif type == 'duplex':
+            CONFIGURE_IN_ANCHOR = 'echo "dummynet in all pipe 1" | pfctl -a '+anchor_name+' -f -'
+            CONFIGURE_OUT_ANCHOR = 'echo "dummynet out all pipe 2" | pfctl -a '+anchor_name+' -f -'
+            output1 = subprocess.check_output(CONFIGURE_IN_ANCHOR, shell=True)
+            output2 = subprocess.check_output(CONFIGURE_OUT_ANCHOR, shell=True)
+            print(output1)
+            print(output2)
+        else:
+            raise Exception("Option not found")
+    except subprocess.CalledProcessError as e:
+            print(e.output)
+            return False
+
+
+
+
+
