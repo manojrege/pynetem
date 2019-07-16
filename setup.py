8dc07e33a533379a7c14a19559caf43768ac2d64
manojrege <manoj.rege@emnify.com>
2019-07-16 16:07:48 +0200

    FIX EOL missing string literal

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     author='Manoj R. Rege',
     author_email='rege.manoj@gmail.com',
-    url='https://github.com/manojrege/pynetem,
+    url='https://github.com/manojrege/pynetem',
     description='A Python wrapper library for network emulation on MacOS',
     long_description=open('README.md').read(),
     license=open('LICENSE').read(),
