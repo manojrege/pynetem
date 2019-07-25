faa1fd39ccb3365141bcbcaa905f2e5318834b21
manojrege <manoj.rege@emnify.com>
2019-07-25 20:02:59 +0200

    Include data directory in the setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='pynetem',
     version='0.1',
     author='Manoj R. Rege',
@@ -9,7 +9,11 @@ setup(
     description='A Python wrapper library for network emulation on MacOS',
     long_description=open('README.md').read(),
     license=open('LICENSE').read(),
-    packages=[
-        'pynetem'
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "LICENSE :: OSI APPROVED :: BSD LICENSE",
     ],
+    package_data = {'pynetem': ['data/dummynet.conf.j2']},
+    include_package_data = True,
 )
